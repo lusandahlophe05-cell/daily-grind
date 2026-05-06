7e42784279329715a880b8cb10a3cd9b0087a456
lusandahlophe05-cell <lusandahlophe05@gmail.com>
2026-05-05 19:10:18 -0700

    first version of coffee object

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,3 +1,18 @@
+/*
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+
+    pic - image src
+    alt - the alt tag for the image
+    desc - a description of the coffee
+    day - the day of the week for the coffee
+    color - color associated with coffee
+    name - the name of the coffee
+
+*/
+
+
 let myDate = new Date();
 let myDay = myDate.getDay();
 let today = " ";
@@ -9,9 +24,19 @@ switch(myDay){
  	break;
 
  	case 2:
-   	today = "Tuesday";
- 	break;
+   	    today = "Tuesday";
+        coffee = {
+            name:"Bubble Tea",
+            Pic:"bubble-tea.jpg",
+            alt:"A pic of a yummy bubble tea.",
+            color:"pink",
+            day:"Tuesday",
+            desc:`I like some yummy bubble tea`
+            
 
+        };
+ 	break;
+console.log(coffee);
  	default:
     	today = "Something went wrong!";
 
